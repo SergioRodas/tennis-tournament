e9a004c47bff424e7d01f566510546827014d24d
Sergio Rodas <sergiorodasgonzalez@gmail.com>
2024-10-11 03:11:04 -0300

    feat(backend): add nelmio/cors-bundle for CORS support

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
