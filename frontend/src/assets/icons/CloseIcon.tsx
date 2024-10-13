97075e4f5bc9abc2946afd0e0f7bc8e85d240644
Sergio Rodas <sergiorodasgonzalez@gmail.com>
2024-10-12 22:52:08 -0300

    feat(ui): add CloseIcon component

---
+++ frontend/src/assets/icons/CloseIcon.tsx
@@ -0,0 +1,16 @@
+export const CloseIcon = ({ className = '' }) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fill="#6b7280"
+        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8L4.646 5.354a.5.5 0 0 1 0-.708"
+      />
+    </svg>
+  );
+};
